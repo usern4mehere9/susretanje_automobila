7dffcdabca34404174cfcd68e7980899ac31b613
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-27 11:56:04 +0200

    Add files via upload

---
+++ Sustizanje_automobila/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sustizanje_automobila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1, v2, t, Rez;
+            v1 = double.Parse(Console.ReadLine());
+            v2 = double.Parse(Console.ReadLine());
+            t = double.Parse(Console.ReadLine());
+
+            t = t * 60;
+            Rez = (v1 * t) / (v2 - v1);
+            Rez = Rez / 60;
+
+            Console.WriteLine(Math.Round(Rez, 2));
+        }
+    }
+}
